5c872493846b75b5b743acbc5dfb91f34768b34e
RC03 <rncall92@gmail.com>
2017-06-23 17:56:06 -0600

    update

--- jsonweather-html/jsonweather-html/scripts/modules.js
+++ jsonweather-html/jsonweather-html/scripts/modules.js
@@ -1,7 +1,7 @@
 // Include modules into the html file
 // Pathes may need altered depending on site structure
 $(function () {
-    $("#page-header").load("/RC03.github.io/jsonweather-html/jsonweather-html/modules/header.html";
+    $("#page-header").load("/RC03.github.io/jsonweather-html/jsonweather-html/modules/header.html");
     $("#page-nav").load("/RC03.github.io/jsonweather-html/jsonweather-html/modules/navigation.html");
     $("#footer-content").load("/RC03.github.io/jsonweather-html/jsonweather-html/modules/footer.html");
 });
